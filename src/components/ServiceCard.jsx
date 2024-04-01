3d2153bc4744c2c153c93ba5fdb4801e01adc4a7
Ucha Kobakhidze <ukobakhidze@geword.local>
2024-04-01 14:30:05 +0400

    minor styling bug fixes

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -7,7 +7,7 @@ export default function ServiceCard(props) {
             <img src={props.img} className={`card-img ${props.imgStyle}`}/>
             <span className='card-date'>{props.date}</span>
             <h3 className='card-title'>{props.title}</h3>
-            <p className='card-description'>{props.desc}</p>
+            <p className={`card-description ${props.descStyle}`}>{props.desc}</p>
             <span className='card-full-info'>View full information <img src={servicearrow} alt="" /></span>
         </div>
     )
