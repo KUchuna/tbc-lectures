c77799c6eb47314d3d27e9844f8176861fab1892
Ucha Kobakhidze <ukobakhidze@geword.local>
2024-04-03 11:53:35 +0400

    done copying all files

---
+++ routes/Blog.jsx
@@ -0,0 +1,87 @@
+import BlogCard from "../components/BlogCard";
+import blogData from "../blogData";
+import '../styles/BlogServices.css'
+import FilterInput from '../components/FilterInput.jsx'
+import React from "react";
+
+export default function Blog() {
+
+    const [open, setOpen] = React.useState(false)
+    const [selection, setSelection] = React.useState("None")
+    const [blogCards, setBlogCards] = React.useState(blogData)
+    const [sorted, setSorted] = React.useState(false)
+
+
+    const mappedBlog = blogCards.map(card => {
+        return <BlogCard 
+                    img={card.img}
+                    date={card.date}
+                    title={card.title}
+                    desc={card.desc}
+                    style='blog-page-card-container'
+                    key={card.id}
+                />
+    })
+
+    function handleClick() {
+        setOpen(prevState => !prevState)
+        
+    }
+
+    function storeSelection(option) {
+        setSelection(option)
+    }
+
+    function handleSort() {
+        console.log(selection)
+        console.log(blogCards)
+        console.log(sorted)
+        if(selection == "Date ascending") {
+            setBlogCards(prevBlogCards => [...prevBlogCards].sort((a, b) => {
+                const dateA = new Date(a.date)
+                const dateB = new Date(b.date)
+                return dateA - dateB
+            }))
+            setSorted(true)
+        }else if(selection == "Date descending") {
+            setBlogCards(prevBlogCards => [...prevBlogCards].sort((a, b) => {
+                const dateA = new Date(a.date)
+                const dateB = new Date(b.date)
+                return dateB - dateA
+            }))
+            setSorted(true)
+        }else if(selection == "None") {
+            setBlogCards(blogData)
+            setSorted(false)
+            setSelection("None")
+        }
+    }
+
+    return (
+        <div className="blog-page-container">
+            <div className="blog-service-page-title-container parent-flex-column-center">
+                <span className="blog-service-page-short-title">BLOG</span>
+                <h1 className="blog-service-page-title">read our latest blog</h1>
+                <p className="blog-service-page-desc">Insights, Tips, and News for Car Enthusiasts</p>
+            </div>
+            <div className="parent-flex-column-center">
+                <div className="sorting-container">
+                    <span>Sort by:</span>
+                        <FilterInput 
+                            dropDownDefault={selection}
+                            sortingStyle='sorting-dropdown-container'
+                            contStyle='sorting-parent-container'
+                            onClick={handleClick}
+                            open={open}
+                            selection={selection}
+                            storeSelection={storeSelection}
+                        />
+                    <button className="sorting-button" onClick={handleSort}>Apply</button>
+                </div>
+                <div className="blog-page-cards-container">
+                    {mappedBlog}
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ routes/Contact.jsx
@@ -0,0 +1,66 @@
+import '../styles/Contact.css'
+import ContactInfo from '../components/ContactInfo'
+import contactemail from '../assets/contactemail.svg'
+import contactlocation from '../assets/contactlocation.svg'
+import contactphone from '../assets/contactphone.svg'
+import ContactInput from '../components/ContactInput.jsx'
+
+
+
+export default function Contact() {
+    return (
+        <div className="contact-page-container parent-flex-column-center">
+            <div className="contact-above-section-container parent-max-width parent-flex-column-center">
+                <div className="contact-title-container">
+                    <span id="contact-us">Contact us</span>
+                    <h1 id='contact-title'>We’d love to hear from you</h1>
+                    <span id='contact-subtitle'>Our friendly team is always here to chat.</span>
+                </div>
+                <div className='contact-info-cards-container'>
+                    <ContactInfo 
+                        img={contactemail}
+                        name='Email'
+                        desc="Our friendly team is here to help."
+                        info="hi@movan.com"
+                    />
+                    <ContactInfo 
+                        img={contactlocation}
+                        name='Office'
+                        desc="Come say hello at our office HQ."
+                        info="100 Smith Street Collingwood VIC 3066 AU"
+                    />
+                    <ContactInfo 
+                        img={contactphone}
+                        name='Phone'
+                        desc="Mon-Fri from 8am to 5pm."
+                        info="+995 555 555 555"
+                    />
+                </div>
+            </div>
+            <div className='contact-below-section-container parent-flex-column-center'>
+                <ContactInput 
+                    label="Full name"
+                    type="text"
+                    placeholder='Full name'
+                />
+                <ContactInput 
+                    label="Email"
+                    type="email"
+                    placeholder='example@example.com'
+                />
+                <ContactInput 
+                    label="Phone number"
+                    type="text"
+                    placeholder='+995 555 555 555'
+                />
+                <ContactInput 
+                    label="Message"
+                    type="text"
+                    textarea
+                    placeholder="Leave us a message..."
+                />
+                <button className='contact-button'>CONTACT</button>
+            </div>
+        </div>
+    )
+}
