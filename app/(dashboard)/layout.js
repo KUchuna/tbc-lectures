82fcbfdaa8ca649dd5d8a84521a30c2d6d2f5ebf
Ucha <uchakobakhidze@gmail.com>
2024-04-25 00:52:24 +0400

    Fix route handling on build

--- app/(dashboard)/layout.js
+++ app/(dashboard)/layout.js
@@ -1,18 +1,8 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import { cookies } from "next/headers"
-import { AUTH_COOKIE_KEY } from "@/constants";
-import { redirect } from "next/navigation";
 
 
 export default function DashboardLayout({children}) {
-    const cookieStore = cookies()
-    const cookie = cookieStore.get(AUTH_COOKIE_KEY)
-
-    if (!cookie?.value) {
-        redirect("/login")
-    }
-
 
     return (
         <>
