272abf2de128f21f3772000629392037d84de639
ucha <uchakobakhidze@gmail.com>
2024-05-19 15:55:43 +0400

    minor styling bug fix

--- components/ShoppingCart.tsx
+++ components/ShoppingCart.tsx
@@ -11,7 +11,7 @@ export const revalidate = 0;
 export default async function ShoppingCart() {
   const cartItems = await getCart();
   return (
-    <div className="min-h-screen bg-slate-700 p-4 flex flex-col items-center">
+    <div className="min-h-screen dark:bg-slate-700 p-4 flex flex-col items-center">
       <ResetBtn />
       <div className="grid grid-cols-3 place-items-center gap-10 dark:bg-slate-700 w-full">
         {cartItems.map((item: Item) => (
