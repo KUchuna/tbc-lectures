8b3aa5d6b7802560126c8146b2a8de942a2ee17e
mariamkhutuashvili <m_khutuashvili@yahoo.com>
2024-05-19 15:33:27 +0400

    style reset button and cart page

--- components/ResetBtn.tsx
+++ components/ResetBtn.tsx
@@ -6,9 +6,9 @@ export default function ResetBtn() {
     await resetCartAction();
   };
   return (
-    <div>
-      <button onClick={handleResetBtn} className="mb-4 w-32 bg-slate-600	">
-        Reset Cart Items
+    <div className="flex justify-center w-full">
+      <button onClick={handleResetBtn} className="mb-4 w-32 bg-slate-600 text-white py-2 px-4 rounded hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-500 transition duration-200">
+        Reset Cart
       </button>
     </div>
   );
