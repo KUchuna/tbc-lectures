f5417298dba256e7afe4c9ee3b57fd0a2ea6e84d
ucha <uchakobakhidze@gmail.com>
2024-06-14 12:29:13 +0400

    fixed but on /bookings if user is not logged in

--- app/[locale]/(dashboard)/bookings/page.tsx
+++ app/[locale]/(dashboard)/bookings/page.tsx
@@ -6,17 +6,19 @@ export default async function Bookings() {
   const data = await getSession()
 
   let auth_id;
+  let bookings;  
 
   if(data) {
     auth_id = data.user.sub
+    bookings = await getBookings(auth_id)
   }
 
-  const bookings = await getBookings(auth_id)
 
   return (
     <div className="min-h-[100vh] dark:bg-slate-700 flex justify-center items-center">
       <section className="overflow-y-auto h-[full] py-[50px]">
-          {bookings.map((booking: any) => <h1 key={booking.id}>{booking.id}</h1>)}
+          {data ? bookings.map((booking: any) => <h1 key={booking.id}>{booking.id}</h1>) : <h1 className="font-bold text-2xl">not signed in</h1>}
+
       </section>
     </div>
   )
