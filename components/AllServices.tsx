5099592348d7ed186ce02cbe55e0307e7f6528e9
ucha <uchakobakhidze@gmail.com>
2024-06-17 17:28:06 +0400

    minor styling changes on /services

--- components/AllServices.tsx
+++ components/AllServices.tsx
@@ -88,7 +88,7 @@ export default function AllServices() {
                        /> })
 
     return (
-        <div className='section-container xl:pt-[96px] md:pt-[40px] py-[30px] px-[16px] md:px-[40px] xl:px-[112px] parent-flex-column-center service-section dark:bg-slate-800'>
+        <div className='xl:pt-[96px] md:pt-[40px] py-[30px] px-[16px] md:px-[40px] xl:px-[112px] parent-flex-column-center service-section dark:bg-slate-800'>
             <div className='parent-max-width'>
                 <div className="section-short-title-link-container"> 
                     <span className="section-short-title">Services</span>
