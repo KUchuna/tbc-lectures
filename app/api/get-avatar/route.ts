1bd2333b1c90c841cd74d3585b6aa647b9317360
ucha <uchakobakhidze@gmail.com>
2024-06-22 02:18:33 +0400

    attempt to fix get-avatar on deployment

--- app/api/get-avatar/route.ts
+++ app/api/get-avatar/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { sql } from '@vercel/postgres';
 
+export const revalidate = 0;
+export const dynamic = 'force-dynamic';
 
 export async function GET(request: NextRequest) {
   try {
